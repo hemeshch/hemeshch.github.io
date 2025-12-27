94a0fa20327ce04fbd71b7aef7f201fd9588da57
Hemesh Chadalavada <55226601+hemeshch@users.noreply.github.com>
2025-12-27 16:52:03 -0600

    new portfolio to capture my chaos v1.2: removed old site files, made images more responsive and fixed some minor text issues

--- assets/index.js
+++
@@ -1,6 +0,0 @@
-const navItems = document.querySelector('#navbar-items')
-const burgerMenu = document.querySelector('#burger-menu')
-
-burgerMenu.addEventListener('click', function () {
-	navItems.classList.toggle('is-active')
-})
